5f01121523e1eed0ce1657296b8051f196c8ff07
tolokh14 <dzulsuyufi@gmail.com>
2022-09-28 15:28:27 +0700

    Added new class

--- class.js
+++ class.js
@@ -8,4 +8,20 @@ class Mail{
         console.log(`you send : ${msg} to ${to} from ${this.from}`);
         this._contacts.push(to);
     }
+
+    showAllContacts(){
+        return this._contacts;
+    }
 }
+
+class WhatsApp extends Mail{
+    constructor(username, isBussinessAccount, phoneNumber){
+        super(phoneNumber);
+        this.username = username;
+        this.isBussinessAccount = isBussinessAccount;
+    }
+}
+
+const wa1 = new WhatsApp('dicoding',true,'6213412312345')
+
+console.log(wa1.from);
